f912adfe4d617ff4b54647fb6c03cd4b1e4f3a7a
Cole Roberts <coleroberts76@gmail.com>
2023-03-07 10:41:58 -0600

    putting in information of models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,11 +1,56 @@
 // import models
+const { Model, DataTypes } = require('sequelize');
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+class Product extends Model {}
 
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true, 
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      Validate: {
+        isNumeric: true, 
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
